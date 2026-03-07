50876cc6a29d0e796109ced8cfc97f2c007d19f2
adriangonar <61589319+adriangonar@users.noreply.github.com>
2026-03-07 21:35:56 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'la-roca-v4';
+const CACHE = 'la-roca-v5';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
